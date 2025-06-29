190b22e850fe7f7f1677e07a392c606301c6851d
Sol!dΛnt0x 🧪🐜 <38260436+0xantman@users.noreply.github.com>
2025-06-29 12:08:08 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export default {
   pythEndpointUrl: "https://hermes.pyth.network",
-  suiRpcUrl: "https://fullnode.mainnet.sui.io:443",
-  deepbookBalanceManagerId: "0xabc123",
-  deepbookPoolName: "LBTC_USDC",
+  suiRpcUrl: "https://api.us1.shinami.com/sui/node/v1/us1_sui_mainnet_5e647a2fd39d4cb585b5603b2a0ba383",
+  deepbookBalanceManagerId: "0xc393e28d06d16a05c4bd60a476bcfab7694ef4d93d44494b1c2e06149a8251ec",
+  deepbookPoolName: "DORI_USDC",
   arbitrageSlippage: 0.01, // 1%
   swapSlippage: 0.01, // 1%
   arbitrageInterval: 30000, // 30 seconds
